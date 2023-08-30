b4507e92c5c624f6f7b6ae258e0293002ae91b3f
Narek Grigoryan <ngrigoryan228@gmail.com>
2023-08-31 01:25:33 +0400

    program

---
+++ src/component/Button/Button.js
@@ -0,0 +1,35 @@
+
+import "./button.css"
+
+const Buttons = () => {
+   
+
+   
+    return (
+        <div>
+            <div class="angry-grid">
+  <div id="item-0">4</div>
+  <div id="item-1">5</div>
+  <div id="item-2">6</div>
+  <div id="item-3">+</div>
+  <div id="item-4">-</div>
+  <div id="item-5">X</div>
+  <div id="item-6">/</div>
+  <div id="item-7">&#10092;</div>
+  <div id="item-8">C</div>
+  <div id="item-9">7</div>
+  <div id="item-10">8</div>
+  <div id="item-11">9</div>
+  <div id="item-12">1</div>
+  <div id="item-13">2</div>
+  <div id="item-14">3</div>
+  <div id="item-15">=</div>
+  <div id="item-16">.</div>
+  <div id="item-17">0</div>
+</div>
+
+           
+        </div>
+    )
+}
+export default Buttons
