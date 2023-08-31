5955584f33c8cdc0b23b0e24d130f7ca8369fdd1
Narek Grigoryan <ngrigoryan228@gmail.com>
2023-08-31 10:36:14 +0400

    update buttons

--- src/component/Button/Button.js
+++ src/component/Button/Button.js
@@ -8,24 +8,24 @@ const Buttons = () => {
     return (
         <div>
             <div class="angry-grid">
-  <div id="item-0">4</div>
-  <div id="item-1">5</div>
-  <div id="item-2">6</div>
-  <div id="item-3">+</div>
-  <div id="item-4">-</div>
-  <div id="item-5">X</div>
-  <div id="item-6">/</div>
-  <div id="item-7">&#10092;</div>
-  <div id="item-8">C</div>
-  <div id="item-9">7</div>
-  <div id="item-10">8</div>
-  <div id="item-11">9</div>
-  <div id="item-12">1</div>
-  <div id="item-13">2</div>
-  <div id="item-14">3</div>
-  <div id="item-15">=</div>
-  <div id="item-16">.</div>
-  <div id="item-17">0</div>
+  <button id="item-0">4</button>
+  <button id="item-1">5</button>
+  <button id="item-2">6</button>
+  <button id="item-3">+</button>
+  <button id="item-4">-</button>
+  <button id="item-5">X</button>
+  <button id="item-6">/</button>
+  <button id="item-7">&#10092;</button>
+  <button id="item-8">C</button>
+  <button id="item-9">7</button>
+  <button id="item-10">8</button>
+  <button id="item-11">9</button>
+  <button id="item-12">1</button>
+  <button id="item-13">2</button>
+  <button id="item-14">3</button>
+  <button id="item-15">=</button>
+  <button id="item-16">.</button>
+  <button id="item-17">0</button>
 </div>
 
            
